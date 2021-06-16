4e9c2d2a125e678154047da6d023cb1039dd7d64
Andreas Kurka <andreas.kurka@gmail.com>
2021-06-17 01:22:42 +0200

    Initial commit

---
+++ XamarinControlBinding/XamarinControlBinding/XamarinControlBinding/Controls/LabeledDecimalBox.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinControlBinding.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LabeledDecimalBox : ContentView
+    {
+        public static readonly BindableProperty LabelTextProperty = BindableProperty.Create(nameof(LabelText), typeof(string), typeof(LabeledDecimalBox), defaultValue: string.Empty, defaultBindingMode: BindingMode.TwoWay, propertyChanged: LabelTextPropertyChanged);
+
+        // Important, for these Bindings we need BindingMode.TwoWay, otherwise the model won't be updated when the entries text changes
+        public static readonly BindableProperty EntryValueProperty = BindableProperty.Create(nameof(EntryValue), typeof(decimal), typeof(LabeledDecimalBox), defaultValue: decimal.Zero, defaultBindingMode: BindingMode.TwoWay, propertyChanged: EntryValuePropertyChanged);
+
+        public decimal EntryValue
+        {
+            get
+            {
+                return (decimal)base.GetValue(EntryValueProperty);
+            }
+            set
+            {
+                base.SetValue(EntryValueProperty, value);
+            }
+        }
+
+        public string LabelText
+        {
+            get
+            {
+                return (string)base.GetValue(LabelTextProperty);
+            }
+            set
+            {
+                base.SetValue(LabelTextProperty, value);
+            }
+        }
+        
+        public LabeledDecimalBox()
+        {
+            InitializeComponent();
+
+            // Important, otherwise the binding on the Text properties of the Label and Entry won't work
+            Label.BindingContext = this;
+            Entry.BindingContext = this;
+        }
+
+        private static void LabelTextPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            LabeledDecimalBox control = (LabeledDecimalBox)bindable;
+            control.Label.Text = (string)newValue;
+        }
+
+        private static void EntryValuePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            LabeledDecimalBox control = (LabeledDecimalBox)bindable;
+            control.Entry.Text = ((decimal)newValue).ToString();
+        }
+
+
+    }
+}
---
+++ XamarinControlBinding/XamarinControlBinding/XamarinControlBinding/ViewModels/MainPageModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamarinControlBinding.ViewModels
+{
+    public class MainPageModel : BaseViewModel
+    {
+        Random r = new Random();
+        decimal soll = 0;
+        string sollText = "Soll-Binding";
+        decimal ist = 0;
+
+        public ICommand NextCommand { get; }
+
+        public decimal Soll
+        {
+            get { return soll; }
+            set
+            {
+                SetProperty(ref soll, value);
+                OnPropertyChanged("Difference");
+            }
+        }
+
+        public string SollText
+        {
+            get { return sollText; }
+            set
+            {
+                SetProperty(ref sollText, value);
+                OnPropertyChanged("Difference");
+            }
+        }
+
+
+        public decimal Ist
+        {
+            get { return ist; }
+            set
+            {
+                SetProperty(ref ist, value);
+                OnPropertyChanged("Difference");
+            }
+        }
+
+        public decimal Difference
+        {
+            get
+            {
+                return soll - ist;
+            }
+            set
+            {
+            }
+        }
+
+        public MainPageModel()
+        {
+            NextCommand = new Command(async () => Next());
+        }
+
+        async void Next()
+        {
+
+            Soll += r.Next(0, 10);
+            Ist += r.Next(0, 10);
+        }
+    }
+}
